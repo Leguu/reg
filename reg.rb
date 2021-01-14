de7ab4287d83e6a3e241b42077c15d795b7897a5
Leguu <33807061+Leguu@users.noreply.github.com>
2021-01-14 17:12:43 -0500

    Added starboard feature

--- reg.rb
+++ reg.rb
@@ -10,17 +10,30 @@ starboard = bot.channel(799_340_653_280_362_567)
 
 names = YAML.load_file('names.yml')
 
+File.new('starred.yml', 'w') unless File.exist?('starred.yml')
+starred = YAML.load_file('starred.yml', fallback: [])
+
 star = '⭐'
 
 bot.reaction_add do |event|
   next if event.emoji.name != star
 
-  next if event.message.reactions.find { |reaction| reaction.name == star }.count != 1
+  next if event.channel.nsfw
+
+  next if event.message.reactions.find { |reaction| reaction.name == star }.count < 2
+
+  next if starred.include?(event.message.id)
+
+  starred.push(event.message.id)
+  File.open('starred.yml', 'w') { |file| file.write(starred.to_yaml) }
 
   link = "https://discord.com/channels/#{event.server.id}/#{event.channel.id}/#{event.message.id}"
 
   starboard.send_embed do |embed|
-    embed.title = "#{names.fetch(event.message.author.id, event.message.author.name)} posted in ##{event.channel.name}"
+    embed.author = Discordrb::Webhooks::EmbedAuthor.new(
+      name: "#{names.fetch(event.message.author.id,
+                           event.message.author.name)} posted in ##{event.channel.name}", icon_url: event.message.user.avatar_url
+    )
 
     if event.message.attachments.count.positive?
       embed.image = Discordrb::Webhooks::EmbedImage.new(url: event.message.attachments[0].url)
@@ -34,13 +47,9 @@ bot.reaction_add do |event|
 
     embed.description = embed.description + "[Link](#{link})"
 
-    embed.colour = 0xff0000
+    embed.colour = event.message.user.color
     embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: event.message.timestamp.ctime)
   end
-end
-
-bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
 end
 
 bot.run
